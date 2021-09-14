0e8e029637946cc4a42d094d7db9f56c924664ab
jmateo <jmateo.cys@gmail.com>
2021-09-14 17:54:16 -0600

    saber

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const respone={
+    "rates": [
+        {
+            "service_name": "canadapost-overnight",
+            "service_code": "ON",
+            "total_price": "1295",
+            "description": "This is the fastest option by far",
+            "currency": "CAD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-2dayground",
+            "service_code": "2D",
+            "total_price": "2934",
+            "currency": "USD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-priorityovernight",
+            "service_code": "1D",
+            "total_price": "3587",
+            "currency": "USD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        }
+    ]
+};
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/getUsers', function(req, res){
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
-        console.log(data);
-        res.end(data);
-    });
-})
+app.get('/hola', (req, res) => res.send(respone));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
