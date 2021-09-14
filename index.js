61b3ccfc340d8dd2f433f1ac85ccddf894697566
jmateo <jmateo.cys@gmail.com>
2021-09-14 17:50:13 -0600

    saber

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data);
+    });
+})
+
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
